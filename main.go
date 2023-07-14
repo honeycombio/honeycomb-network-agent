4f9e454f87006dcac0375916256e8baf72c41132
Mike Goldsmith <goldsmith.mike@gmail.com>
2023-07-14 21:33:54 +0100

    feat: Print agent and host agent version during startup (#20)
    ## Which problem is this PR solving?
    
    Adds a utility to get the host kernel version and print it out during
    process start up.
    
    ---------
    
    Co-authored-by: Jamie Danielson <jamieedanielson@gmail.com>

--- main.go
+++ main.go
@@ -5,10 +5,22 @@ import (
 	"os"
 
 	"github.com/honeycombio/ebpf-agent/bpf/probes"
+	"github.com/honeycombio/ebpf-agent/utils"
 	"github.com/honeycombio/libhoney-go"
 )
 
+const Version string = "0.0.1"
+
 func main() {
+	log.Printf("Starting Honeycomb eBPF agent v%s\n", Version)
+
+	// Try to detect host kernel kernelVersion
+	kernelVersion, err := utils.HostKernelVersion()
+	if err != nil {
+		log.Fatalf("Failed to get host kernel version: %v", err)
+	}
+	log.Printf("Host kernel version: %s\n", kernelVersion)
+
 	// setup libhoney
 	libhoney.Init(libhoney.Config{
 		APIKey: os.Getenv("HONEYCOMB_API_KEY"),
@@ -16,6 +28,5 @@ func main() {
 	})
 	defer libhoney.Close()
 
-	log.Println("Starting Honeypot eBPF Agent")
 	probes.Setup()
 }
