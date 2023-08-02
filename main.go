c9fd0f0d1605e018a03b7c8769ddd0ce3e8e0dc6
JamieDanielson <jamieedanielson@gmail.com>
2023-08-02 19:12:28 -0400

    use separate version file

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -9,19 +10,12 @@ import (
 	"github.com/honeycombio/libhoney-go"
 )
 
-const Version string = "0.0.1"
 const defaultDataset = "hny-ebpf-agent"
 const defaultEndpoint = "https://api.honeycomb.io"
 
 func main() {
-	log.Printf("Starting Honeycomb eBPF agent v%s\n", Version)
+	log.Printf("Starting Honeycomb eBPF agent v%s\n", utils.Version())
 
-	// Try to detect host kernel kernelVersion
-	kernelVersion, err := utils.HostKernelVersion()
-	if err != nil {
-		log.Fatalf("Failed to get host kernel version: %v", err)
-	}
-	log.Printf("Host kernel version: %s\n", kernelVersion)
 
 	apikey := os.Getenv("HONEYCOMB_API_KEY")
 	if apikey == "" {
@@ -40,6 +34,8 @@ func main() {
 		Dataset: dataset,
 		APIHost: endpoint,
 	})
+	// appends libhoney's user-agent (TODO: doesn't work, no useragent right now)
+	libhoney.UserAgentAddition = fmt.Sprintf("hny/ebpf-agent/%s", utils.Version())
 	defer libhoney.Close()
 
 	// setup probes
---
+++ utils/version.go
@@ -0,0 +1,6 @@
+package utils
+
+// Version is the current version of the ebpf agent
+func Version() string {
+	return "0.0.2"
+}
