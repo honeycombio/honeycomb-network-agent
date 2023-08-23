2070772e5d3e05aa59681855d175ab03a6d71df2
Mike Goldsmth <goldsmith.mike@gmail.com>
2023-08-23 18:32:05 +0100

    move request counter to separate file

--- assemblers/tcp_stream.go
+++ assemblers/tcp_stream.go
@@ -10,28 +10,6 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-type requestCounter struct {
-	requests uint64
-	respones uint64
-	sync.Mutex
-}
-
-func (c *requestCounter) incrementRequest() uint64 {
-	c.Lock()
-	defer c.Unlock()
-
-	c.requests++
-	return c.requests
-}
-
-func (c *requestCounter) incrementResponse() uint64 {
-	c.Lock()
-	defer c.Unlock()
-
-	c.respones++
-	return c.respones
-}
-
 type tcpStream struct {
 	id             uint64
 	tcpstate       *reassembly.TCPSimpleFSM
