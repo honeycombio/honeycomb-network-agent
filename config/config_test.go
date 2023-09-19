719c7d215bf304394d9f41c850bf7948c97b63dc
Mike Goldsmth <goldsmith.mike@gmail.com>
2023-09-19 12:13:20 +0100

    add tests for validating env vars

--- config/config_test.go
+++ config/config_test.go
@@ -46,3 +46,22 @@ func TestAPIMask(t *testing.T) {
 		})
 	}
 }
+
+func TestEnvVars(t *testing.T) {
+	t.Setenv("HONEYCOMB_API_KEY", "1234567890123456789012")
+	t.Setenv("HONEYCOMB_API_ENDPOINT", "https://api.example.com")
+	t.Setenv("HONEYCOMB_DATASET", "test-dataset")
+	t.Setenv("HONEYCOMB_STATS_DATASET", "test-stats-dataset")
+	t.Setenv("LOG_LEVEL", "DEBUG")
+	t.Setenv("DEBUG", "true")
+	t.Setenv("DEBUG_ADDRESS", "1.2.3.4:5678")
+
+	config := config.NewConfig()
+	assert.Equal(t, "1234567890123456789012", config.APIKey)
+	assert.Equal(t, "https://api.example.com", config.Endpoint)
+	assert.Equal(t, "test-dataset", config.Dataset)
+	assert.Equal(t, "test-stats-dataset", config.StatsDataset)
+	assert.Equal(t, "DEBUG", config.LogLevel)
+	assert.Equal(t, true, config.Debug)
+	assert.Equal(t, "1.2.3.4:5678", config.DebugAddress)
+}
