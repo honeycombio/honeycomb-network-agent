2769b30b7beb1dcd4b4d7026881c26c2f04b54e9
Jamie Danielson <jamiedanielson@honeycomb.io>
2023-10-19 07:16:21 -0400

    fix: add headers to honeycomb event (#289)
    ## Which problem is this PR solving?
    
    - Closes #288 
    
    ## Short description of the changes
    
    - add fields for `http.request.headers` and `http.response.headers` and
    accompanying unit tests. #277 added all the logic for configuring the
    `HTTP_HEADERS` to be extracted, and this PR adds the headers to the
    actual event to be sent to Honeycomb.
    
    This is the smaller implementation mentioned in the issue. We could
    (/should?) have individual attributes for each key, but I figured this
    could be a faster fix to start since we've already published with the
    expectation of HTTP_HEADERS working.
    
    ## How to verify that this has the expected result
    
    Run the agent with something like the example greeting service and see
    the attributes in Honeycomb.
    
    The default captures just the User-Agent:
    
    - `http.request.headers`
      - example: `"{\"User-Agent\":[\"Go-http-client/1.1\"]}"`
    - `http.response.headers`
      - example: `"{}"`
    
    If for example the list includes
    `"User-Agent,Accept-Encoding,Content-Length,Content-Type"`, the result
    looks like this:
    
    - `http.request.headers`
    - example:
    `"{\"Accept-Encoding\":[\"gzip\"],\"User-Agent\":[\"Go-http-client/1.1\"]}"`
    - `http.response.headers`
    - example:
    `"{\"Content-Length\":[\"12\"],\"Content-Type\":[\"text/plain;
    charset=utf-8\"]}"`

--- handlers/libhoney_event_handler.go
+++ handlers/libhoney_event_handler.go
@@ -180,6 +180,9 @@ func (handler *libhoneyEventHandler) addHttpFields(ev *libhoney.Event, event *as
 				ev.AddField(string(semconv.URLPathKey), url.Path)
 			}
 		}
+		// by this point, we've already extracted headers based on HTTP_HEADERS list
+		// so we can safely add the headers to the event
+		ev.AddField("http.request.headers", event.Request().Header)
 	} else {
 		ev.AddField("name", "HTTP")
 		ev.AddField("http.request.missing", "no request on this event")
@@ -198,6 +201,9 @@ func (handler *libhoneyEventHandler) addHttpFields(ev *libhoney.Event, event *as
 			ev.AddField("error", "HTTP client error")
 		}
 		ev.AddField(string(semconv.HTTPResponseBodySizeKey), event.Response().ContentLength)
+		// by this point, we've already extracted headers based on HTTP_HEADERS list
+		// so we can safely add the headers to the event
+		ev.AddField("http.response.headers", event.Response().Header)
 	} else {
 		ev.AddField("http.response.missing", "no response on this event")
 	}
