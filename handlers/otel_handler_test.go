b4e608bb64936a8c6b28c12f48de0c49c6d448e8
Robb Kidd <robbkidd@honeycomb.io>
2023-10-25 10:48:34 -0400

    twiddled test to remove magic strings

--- handlers/otel_handler_test.go
+++ handlers/otel_handler_test.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 	"time"
@@ -23,19 +24,22 @@ func Test_extractContextFromEvent(t *testing.T) {
 		"").(*otelHandler)
 	defer handler.Close()
 
+	traceID, _ := trace.TraceIDFromHex("0af7651916cd43dd8448eb211c80319c")
+	spanID, _ := trace.SpanIDFromHex("b7ad6b7169203331")
+
 	// create a test http event
 	now := time.Now()
 	event := createTestHttpEventWithRequestHeader(now, now, &http.Header{
-		"Traceparent": []string{"00-0af7651916cd43dd8448eb211c80319c-b7ad6b7169203331-01"},
+		"Traceparent": []string{fmt.Sprintf("00-%s-%s-01", traceID, spanID)},
 	})
 
-	// extract the context from the event and ensure it matches the expected values
+	// use handler to get context from the HTTP event
 	ctx := handler.getContextFromEvent(event)
+	// get the span context out of the full event context
 	spanCtx := trace.SpanContextFromContext(ctx)
-	traceID, _ := trace.TraceIDFromHex("0af7651916cd43dd8448eb211c80319c")
-	assert.Equal(t, spanCtx.TraceID(), traceID)
-	spanID, _ := trace.SpanIDFromHex("b7ad6b7169203331")
-	assert.Equal(t, spanCtx.SpanID(), spanID)
+
+	assert.Equal(t, traceID, spanCtx.TraceID())
+	assert.Equal(t, spanID, spanCtx.SpanID())
 }
 
 // test that headerToAttributes correctly sets attributes from headers
